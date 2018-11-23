1b82d2ed5d4b22dd45c6037fc7789d420c195ff4
Cruz <cruzdiary@gmail.com>
2018-11-23 23:08:15 +0900

    Add base project

---
+++ Rx2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rx2048
+//
+//  Created by Cruz on 23/11/2018.
+//  Copyright © 2018 Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
